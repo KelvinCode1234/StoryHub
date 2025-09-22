294959ec85589dae419e5e512ad28fc8841d8fd2
Precious Kelvin.N <precykelly345@gmail.com>
2025-09-22 15:58:20 +0100

    bs-config.js added for url hide

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ window.addEventListener('click', (event) => {
 
     if (intersects.length > 0) {
         // Redirect to the story page
-        window.location.href = 'story.html';
+        window.location.href = 'story';
     }
 });
 
